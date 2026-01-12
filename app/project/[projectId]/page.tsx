f204aebfcc1489be7f44220995225d8adf45af48
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-12 12:53:40 +0500

    Add settings section UI to project canvas page

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ProjectHeader from "./_shared/ProjectHeader";
+import SettingsSection from "./_shared/SettingsSection";
 
 function ProjectCanvasPlayground() {
   return (
     <div>
       <ProjectHeader />
+
+      <div>
+        {/* Settings */}
+        <SettingsSection />
+
+        {/* Canvas */}
+      </div>
     </div>
   );
 }
